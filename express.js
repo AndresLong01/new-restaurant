410fcf291007d946b6056a08a362fdb3d4767d28
misaelalamillo <misaelalamillo@gmail.com>
2020-03-04 13:37:33 -0600

    third commit

--- express.js
+++ express.js
@@ -12,7 +12,18 @@
  app.get("/", function(req, res) {
      res.sendFile(path.join(__dirname, "index.html"));
  });
-
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+app.get("/api/tables", function(req, res) {
+    return res.json(array1);
+});
+app.get("/api/wait", function(req, res) {
+    return res.json(array1);
+});
  app.listen(PORT, function(){
      
- })
+ });
