7594e9865cf20ebe9e7ac6cc9ea74ed14b75847c
Andres Long <andreslong95@gmail.com>
2020-03-04 13:37:45 -0600

    Package and post methods plus runtime enabled

--- express.js
+++ express.js
@@ -1,18 +1,24 @@
- var express = require("express");
- var path = require("path");
+var express = require("express");
+var path = require("path");
 
- var app = express();
- var PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = process.env.PORT || 8080;
 
- app.use(express.urlencoded({ extended: true}));
- app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
  
- let array1 = [];
+let array1 = [];
 
- app.get("/", function(req, res) {
+app.get("/", function(req, res) {
      res.sendFile(path.join(__dirname, "index.html"));
- });
+});
 
- app.listen(PORT, function(){
-     
- })
+app.post("/reserve", function(req, res){
+    let newReservation = req.body;
+    array1.push(newReservation);
+    res.json(newReservation);
+});
+
+app.listen(PORT, function(){
+     console.log("I'm alive!");
+})
