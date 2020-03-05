7727e089f9b23dd8f9dcd229f410665259363ad9
Andres Long <andreslong95@gmail.com>
2020-03-05 10:07:41 -0600

    some Changes

--- express.js
+++ express.js
@@ -8,15 +8,21 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
  
 let array1 = [];
-
+let array2 = [];
 app.get("/", function(req, res) {
      res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/reserve", function(req, res){
     let newReservation = req.body;
-    array1.push(newReservation);
-    res.json(newReservation);
+    let newWaitlist = req.body;
+    if (array1.length <= 5){
+        array1.push(newReservation);
+        res.json(newReservation);
+    } else {
+        array2.push(newWaitlist);
+        res.json(newWaitlist);
+    }
 });
 
 app.get("/tables", function(req, res) {
@@ -28,8 +34,8 @@ app.get("/reserve", function(req, res) {
 app.get("/api/tables", function(req, res) {
     return res.json(array1);
 });
-app.get("/api/wait", function(req, res) {
-    return res.json(array1);
+app.get("/api/waitlist", function(req, res) {
+    return res.json(array2);
 });
  app.listen(PORT, function(){
      console.log("I'm alive on port " + PORT)
